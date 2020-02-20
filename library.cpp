70f297851a2a27a8d7689c3d4aaafc20851ab21e
michaeloed <7830883+michaeloed@users.noreply.github.com>
2020-02-20 06:42:18 +0100

    Initial commit

---
+++ library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 15.02.2020 09:20:19
+ * Author : micha
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
